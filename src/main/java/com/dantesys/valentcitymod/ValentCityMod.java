1cc73d087082161372aee01e0c6115d815f7fec5
dantesy <hotuios@hotmail.com>
2024-09-02 11:27:03 -0300

    Configurando

---
+++ src/main/java/com/dantesys/valentcitymod/ValentCityMod.java
@@ -0,0 +1,29 @@
+package com.dantesys.valentcitymod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(ValentCityMod.MODID)
+public class ValentCityMod
+{
+    public static final String MODID = "valentcitymod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public ValentCityMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+}
