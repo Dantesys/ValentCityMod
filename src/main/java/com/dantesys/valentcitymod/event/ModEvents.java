d2254b9557e9f5cc0443230d0266868701c752c3
dantesy <hotuios@hotmail.com>
2024-09-11 08:53:19 -0300

    Começando a adicionar as reliquias e os efeitos

--- src/main/java/com/dantesys/valentcitymod/event/ModEvents.java
+++ src/main/java/com/dantesys/valentcitymod/event/ModEvents.java
@@ -3,19 +3,17 @@ package com.dantesys.valentcitymod.event;
 import com.dantesys.valentcitymod.ValentCityMod;
 import com.dantesys.valentcitymod.item.ModItens;
 import net.minecraft.ChatFormatting;
-import net.minecraft.server.ServerScoreboard;
-import net.minecraft.util.CommonColors;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.scores.PlayerTeam;
 import net.minecraft.world.scores.Scoreboard;
-import net.minecraft.world.scores.Team;
 import net.minecraftforge.event.entity.living.LivingSwapItemsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -26,18 +24,15 @@ public class ModEvents {
         @SubscribeEvent
         public void segurar(LivingSwapItemsEvent event){
             ItemStack i = event.getEntity().getMainHandItem();
+            LivingEntity le = event.getEntity();
             if(i.is(ModItens.CEIFADOR.get())){
-                event.getEntity().addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION,-1));
-                event.getEntity().addEffect(new MobEffectInstance(MobEffects.INVISIBILITY,-1));
-                glowColor(event.getEntity(), ChatFormatting.BLACK);
+                le.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION,-1));
+                le.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY,-1));
+                glowColor(le, ChatFormatting.BLACK);
+            }else{
+                stopGlowing(le);
+                limpaEfeito(le);
             }
-        }
-    }
-    @Mod.EventBusSubscriber(modid = ValentCityMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ModEventBusEvents {
-        @SubscribeEvent
-        public static void segurar(LivingSwapItemsEvent event) {
-
         }
     }
     public static void glowColor(LivingEntity player, ChatFormatting color) {
@@ -48,11 +43,21 @@ public class ModEvents {
         tempTeams.put(player, team);
         player.addEffect(new MobEffectInstance(MobEffects.GLOWING,-1));
     }
-    public void stopGlowing(LivingEntity player) {
+    public static void stopGlowing(LivingEntity player) {
         player.removeEffect(MobEffects.GLOWING);
         PlayerTeam team = tempTeams.get(player);
         team.getScoreboard().removePlayerFromTeam(player.getName().getString());
         team.getScoreboard().removePlayerTeam(team);
         tempTeams.remove(player);
+    }
+    public static void limpaEfeito(LivingEntity player){
+        Collection<MobEffectInstance> efeitos = player.getActiveEffects();
+        while(efeitos.iterator().hasNext()){
+            MobEffectInstance efeito = efeitos.iterator().next();
+            if(efeito.getDuration()<=0){
+                player.removeEffect(efeito.getEffect());
+            }
+            efeitos.iterator().remove();
+        }
     }
 }
