533298a2ea916e1f411b5cf0f9cfebbaf8ad303d
dantesy <hotuios@hotmail.com>
2024-09-23 09:40:49 -0300

    ajustando eventos da reliquia do ceifador

--- src/main/java/com/dantesys/valentcitymod/ValentCityMod.java
+++ src/main/java/com/dantesys/valentcitymod/ValentCityMod.java
@@ -1,19 +1,13 @@
 package com.dantesys.valentcitymod;
 
 import com.dantesys.valentcitymod.block.ModBlocks;
-import com.dantesys.valentcitymod.event.ModEvents;
 import com.dantesys.valentcitymod.item.ModItems;
 import com.dantesys.valentcitymod.util.CreativeTab;
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -26,8 +20,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.RegisterEvent;
 import org.slf4j.Logger;
 
-import java.util.Collection;
-
 @Mod(ValentCityMod.MODID)
 public class ValentCityMod
 {
@@ -39,7 +31,6 @@ public class ValentCityMod
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::addCreativeTba);
         MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.register(ModEvents.class);
         CreativeTab.register(modEventBus);
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
@@ -68,25 +59,6 @@ public class ValentCityMod
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
 
-        }
-        @SubscribeEvent
-        public static void segurar(TickEvent.PlayerTickEvent event){
-            Player le = event.player;
-            ItemStack i = event.player.getMainHandItem();
-            if(i.is(ModItems.CEIFADORPR.get()) || i.is(ModItems.CEIFADORR.get())){
-                le.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION,-1));
-                le.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY,-1));
-                le.addEffect(new MobEffectInstance(MobEffects.GLOWING,-1));
-            }else{
-                Collection<MobEffectInstance> efeitos = le.getActiveEffects();
-                while(!efeitos.isEmpty()){
-                    MobEffectInstance efeito = efeitos.iterator().next();
-                    efeitos.remove(efeito);
-                    if(efeito != null && efeito.getDuration()==-1){
-                        le.removeEffect(efeito.getEffect());
-                    }
-                }
-            }
         }
     }
 }
