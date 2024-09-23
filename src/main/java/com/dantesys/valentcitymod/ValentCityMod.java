2512139f5b3d6b9f6bbe135d0fe8ca68be005027
dantesy <hotuios@hotmail.com>
2024-09-23 09:31:20 -0300

    ajustando eventos da reliquia do ceifador

--- src/main/java/com/dantesys/valentcitymod/ValentCityMod.java
+++ src/main/java/com/dantesys/valentcitymod/ValentCityMod.java
@@ -5,10 +5,15 @@ import com.dantesys.valentcitymod.event.ModEvents;
 import com.dantesys.valentcitymod.item.ModItems;
 import com.dantesys.valentcitymod.util.CreativeTab;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -20,6 +25,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.RegisterEvent;
 import org.slf4j.Logger;
+
+import java.util.Collection;
 
 @Mod(ValentCityMod.MODID)
 public class ValentCityMod
@@ -61,6 +68,25 @@ public class ValentCityMod
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
 
+        }
+        @SubscribeEvent
+        public static void segurar(TickEvent.PlayerTickEvent event){
+            Player le = event.player;
+            ItemStack i = event.player.getMainHandItem();
+            if(i.is(ModItems.CEIFADORPR.get()) || i.is(ModItems.CEIFADORR.get())){
+                le.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION,-1));
+                le.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY,-1));
+                le.addEffect(new MobEffectInstance(MobEffects.GLOWING,-1));
+            }else{
+                Collection<MobEffectInstance> efeitos = le.getActiveEffects();
+                while(!efeitos.isEmpty()){
+                    MobEffectInstance efeito = efeitos.iterator().next();
+                    efeitos.remove(efeito);
+                    if(efeito != null && efeito.getDuration()==-1){
+                        le.removeEffect(efeito.getEffect());
+                    }
+                }
+            }
         }
     }
 }
--- src/main/java/com/dantesys/valentcitymod/event/ModEvents.java
+++ src/main/java/com/dantesys/valentcitymod/event/ModEvents.java
@@ -3,46 +3,19 @@ package com.dantesys.valentcitymod.event;
 import com.dantesys.valentcitymod.ValentCityMod;
 import com.dantesys.valentcitymod.item.ModItems;
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
-
-import java.util.Collection;
 import java.util.Objects;
 
 @Mod.EventBusSubscriber(modid = ValentCityMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEvents {
     private static final Logger LOGGER = LogUtils.getLogger();
-    @SubscribeEvent
-    public static void segurar(TickEvent.PlayerTickEvent event){
-        Player le = event.player;
-        ItemStack i = event.player.getMainHandItem();
-        if(i.is(ModItems.CEIFADORPR.get()) || i.is(ModItems.CEIFADORR.get())){
-            le.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION,-1));
-            le.addEffect(new MobEffectInstance(MobEffects.INVISIBILITY,-1));
-            le.addEffect(new MobEffectInstance(MobEffects.GLOWING,-1));
-        }else{
-            Collection<MobEffectInstance> efeitos = le.getActiveEffects();
-            while(!efeitos.isEmpty()){
-                MobEffectInstance efeito = efeitos.iterator().next();
-                efeitos.remove(efeito);
-                if(efeito != null && efeito.getDuration()==-1){
-                    le.removeEffect(efeito.getEffect());
-                }
-            }
-        }
-    }
     @SubscribeEvent
     public void foidef(LivingDeathEvent event){
         Entity fontedano = event.getSource().getEntity();
