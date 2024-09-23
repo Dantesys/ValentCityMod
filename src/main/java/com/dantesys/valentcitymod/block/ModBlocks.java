2b8aafb3b168ffe0da77a4782b1446d720a1c1a6
dantesy <hotuios@hotmail.com>
2024-09-23 09:22:06 -0300

    ajustando eventos da reliquia do ceifador

--- src/main/java/com/dantesys/valentcitymod/block/ModBlocks.java
+++ src/main/java/com/dantesys/valentcitymod/block/ModBlocks.java
@@ -6,6 +6,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.HeavyCoreBlock;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,7 +18,7 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ValentCityMod.MODID);
 
     public static final RegistryObject<Block> ASCENSION_CORE = registerBlock("ascension_core",
-            () -> new Block(Blocks.HEAVY_CORE.properties()));
+            () -> new HeavyCoreBlock(Blocks.HEAVY_CORE.properties()));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
         RegistryObject<T> retorno = BLOCKS.register(name,block);
